4597fd6021ed48fb39bdcd9a92764a937e8d733b
Adam <adamrh48@outlook.com>
2025-09-14 22:56:24 +0100

    Started layout reorganisation

--- water_watch/lib/main.dart
+++ water_watch/lib/main.dart
@@ -67,16 +67,41 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      // TODO RESUME
+      // FIND A WAY OF COORDINATING THE ELEMENTS OF THE WEBPAGE
       body: Center(
         child: Column(
           children: [
-            // TODO
-            //  EXPERIMENT WITH MARKERS
             SizedBox(
               width: MediaQuery.of(context).size.width,
-              // height: MediaQuery.of(context).size.height,
+              height: MediaQuery.of(context).size.height * 0.85,
               child: InteractiveViewer(
                 maxScale: 75.0,
+                // TODO - ON HOVER CHANGE CURSOR TO POINTER?
+                /* https://api.flutter.dev/flutter/services/MouseCursor-class.html
+                 * DO SOMETHING LIKE THIS
+                 * WRAP THE ENTIRE LAYOUT IN A MOUSEREGION
+                 * SET THE CURSOR PROPERTY TO A VARIABLE
+                 * CHANGE THE CURSOR PROPERTY TO POINTER IN THE ONHOVER CALLBACK
+                 * OF THE MAP, SO WE SEE A CURSOR POINTER WHEN HOVERING ON A COUNTY?
+                 *   @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.text,
+        child: Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(color: Colors.yellow),
+          ),
+        ),
+      ),
+    );
+  }
+                 *
+                 */
                 child: SimpleMap(
                   instructions: SMapUnitedKingdom.instructions,
                   countryBorder: CountryBorder(color: Colors.white),
