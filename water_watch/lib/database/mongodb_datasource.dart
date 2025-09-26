45f31000631e087a4239c7c3134ddae243c96108
Adam <adamrh48@outlook.com>
2025-09-26 23:19:31 +0100

    Added MongoDB credentials

--- water_watch/lib/database/mongodb_datasource.dart
+++ water_watch/lib/database/mongodb_datasource.dart
@@ -1,18 +1,21 @@
 import 'dart:developer';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
+/*
+ * DOCUMENTATION
+ *
+ * - https://medium.com/@umkithya/storing-credentials-securely-in-a-flutter-project-c9d22dcf7584
+ */
 class MongoDB {
   static Db? db;
   static DbCollection? userCollection;
 
   static Future<void> connect() async {
     try {
-      // TODO - Find how to add password
-      // https://medium.com/@umkithya/storing-credentials-securely-in-a-flutter-project-c9d22dcf7584
-
-      // TODO - DELEGATE BELOW STRING AND PASSWORD GENERATION TO HELPER METHOD
-      db = await Db.create("mongodb+srv://water_watch_admin:<db_password>@maincluster.smtynbd.mongodb.net/?retryWrites=true&w=majority&appName=MainCluster");
+      final String connectionString = await getConnectionString();
+      db = await Db.create(connectionString);
       await db!.open();
       userCollection = db!.collection("water_watch.drought_status");
       log('Connected to MongoDB');
@@ -25,5 +28,14 @@ class MongoDB {
     final arrData = await userCollection!.find().toList();
     log('Data in collection: ${arrData.toString()}');
     return arrData;
+  }
+
+  static Future<String> getConnectionString() async {
+    await dotenv.load(fileName: ".env");
+    final username = dotenv.env['DB_USERNAME'];
+    final password = dotenv.env['DB_PASSWORD'];
+    return """mongodb+srv://${username!}:${password!}@maincluster
+    .smtynbd.mongodb.net/?retryWrites=true
+    &w=majority&appName=MainCluster""";
   }
 }
