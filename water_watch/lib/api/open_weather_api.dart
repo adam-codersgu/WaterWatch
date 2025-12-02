13f89ed8a5072da7153dc73531d62aada2836d0b
Adam <adamrh48@outlook.com>
2025-12-02 21:14:20 +0000

    Sending API requests

--- water_watch/lib/api/open_weather_api.dart
+++ water_watch/lib/api/open_weather_api.dart
@@ -1,18 +1,29 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:water_watch/constants/city_coordinates.dart';
 
 /*
 DOCUMENTATION
  - https://docs.flutter.dev/cookbook/networking/fetch-data
  */
 class OpenWeatherAPI {
-  static const String SIXTEEN_DAY_FORECAST_URL = "api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=16&appid=";
+  static const String FIVE_DAY_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
   // TODO RESUME - CALL API FOR BRIGHTON/LONDON
+
+  static Future<http.Response> getBrightonWeather() async {
+    String requestUrl = appendLatLon(FIVE_DAY_FORECAST_URL, brightonLatLon);
+    requestUrl = await appendApiKey(requestUrl);
+    return http.get(Uri.parse(requestUrl));
+  }
+
+  static String appendLatLon(final String requestUrl, final (double, double) coordinates) {
+    return "$requestUrl&lat=${coordinates.$1}&lon=${coordinates.$2}";
+  }
 
   static Future<String> appendApiKey(final String requestUrl) async {
     await dotenv.load(fileName: ".env");
     final apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
-    return requestUrl + apiKey!;
+    return "$requestUrl&appId=$apiKey";
   }
 }
--- water_watch/lib/constants/city_coordinates.dart
+++ water_watch/lib/constants/city_coordinates.dart
@@ -1,7 +1,7 @@
 /// GB-UKI Greater London
 /// Hertfordshire and North London (hnl)
-const Record northLondonLatLon = (51.5072, 0.1276);
+const (double, double) northLondonLatLon = (51.5072, 0.1276);
 
 /// GB-UKJ South East
 /// Solent and South Downs (ssd)
-const Record brightonLatLon = (50.8229, 0.1363);
+const (double, double) brightonLatLon = (50.8229, 0.1363);
