0b7a1013d8fbd0f9001409f48fb95b1d20feb399
Adam <adamrh48@outlook.com>
2025-12-08 20:05:37 +0000

    Display API output

--- water_watch/lib/dialog/rain_status_dialog.dart
+++ water_watch/lib/dialog/rain_status_dialog.dart
@@ -1,13 +1,11 @@
+import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 import '../api/open_weather_api.dart';
-import '../model/drought.dart';
 import '../model/drought_status.dart';
-import '../model/drought_status_description.dart';
+import '../model/five_day_forecast.dart';
 
 /*
 TODO - ULTIMATELY REPLACE THIS WIDGET WITH A DEDICATED PAGE WITH DATA ON IT
@@ -22,7 +20,6 @@ class RainAmountWidget extends StatefulWidget {
 }
 
 class _RainAmountWidgetState extends State<RainAmountWidget> {
-
   static const String _coordinatesDelimiter = ",";
   double rainAmount = 0.00;
 
@@ -31,37 +28,51 @@ class _RainAmountWidgetState extends State<RainAmountWidget> {
     super.initState();
 
     final List<(double, double)> latLonCoordinates = [];
-    widget.droughtStatus.coordinates?.forEach(
-          (coordinatePair) {
-            final List<String> coordinatesStringList = coordinatePair.split(_coordinatesDelimiter);
-            if (coordinatesStringList.length != 2) {
-              log('Error: The coordinates list contains ${coordinatesStringList.length} elements. '
-                  'Expected 2.');
-              return;
-            }
-            final (double, double) latLon = (double.parse(coordinatesStringList[0]), double.parse(coordinatesStringList[1]));
-            latLonCoordinates.add(latLon);
-          },
-    );
+    widget.droughtStatus.coordinates?.forEach((coordinatePair) {
+      final List<String> coordinatesStringList = coordinatePair.split(
+        _coordinatesDelimiter,
+      );
+      if (coordinatesStringList.length != 2) {
+        log(
+          'Error: The coordinates list contains ${coordinatesStringList.length} elements. '
+          'Expected 2.',
+        );
+        return;
+      }
+      final (double, double) latLon = (
+        double.parse(coordinatesStringList[0]),
+        double.parse(coordinatesStringList[1]),
+      );
+      latLonCoordinates.add(latLon);
+    });
 
+    // TODO - RESUME - NEED TO TEST THIS
+    double accumulatedRain = 0.00;
     for (var latLon in latLonCoordinates) {
-      // TODO - RESUME - NEED TO GET THE WEATHER FOR LATLONS, SUM THE RAINFALL EXPECATIONS
-      // TODO - RESUME - DIVIDE THE SUM RAINFALL EXPECTATION BY THE NUMBER OF LATLONS
-      // TODO - RESUME - UPDATE THE STATE WITH THE AVERAGE RAINFALL EXPECTATION
-      // TODO - RESUME - NOTE IF UPDATING rainAmount DOES NOT UPDATE STATE, MAY NEED TO UPDATE THE WHOLE TEXT STRING?
-            OpenWeatherAPI.getWeather(latLon).then((result) {
-              // todo - have success and error pathways based on status?
-              final status = result.statusCode;
-              final res = result.body;
-              final json = jsonDecode(res) as Map<String, dynamic>;
-              final FiveDayForecast forecast = FiveDayForecast.fromJson(json);
-            });
-          }
+      OpenWeatherAPI.getWeather(latLon).then((result) {
+        if (result.statusCode == 200) {
+          final res = result.body;
+          final json = jsonDecode(res) as Map<String, dynamic>;
+          final FiveDayForecast forecast = FiveDayForecast.fromJson(json);
+          accumulatedRain += forecast.accumulatedRain;
+        } else {
+          log(
+            'Unsuccessful response status ${result.statusCode} received for '
+            'LatLon $latLon',
+          );
+          return;
+        }
+      });
+    }
+
+    // FIXME - NOTE IF UPDATING rainAmount DOES NOT UPDATE STATE, MAY NEED TO UPDATE THE WHOLE TEXT STRING?
+    setState(() {
+      rainAmount = accumulatedRain / latLonCoordinates.length;
+    });
   }
 
   @override
   Widget build(final BuildContext context) {
-
     final List<Widget> statusWidgets = [
       Text(
         widget.droughtStatus.name,
@@ -93,7 +104,7 @@ class _RainAmountWidgetState extends State<RainAmountWidget> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
-            children: statusWidgets
+            children: statusWidgets,
           ),
         ),
       ),
